eaf757e3d2cb1fbcb409039f36f1b207ab69ed58
Rashmi Andhale <rashmiandhale23@gmail.com>
2026-06-26 23:27:44 +0530

    Set Matrix Zeroes: Optimal Solution

--- 0073_Set_Matrix_Zeroes.py
+++ 0073_Set_Matrix_Zeroes.py
@@ -55,3 +55,40 @@ class Solution:
             for j in range (n):
                 if row[i] == 1 or col[j] == 1:
                     matrix[i][j] = 0
+
+
+
+
+# Optimal Approach
+# Time Complexity: O(m*n) where m is the number of rows and n is the number of columns in the input matrix
+# Space Complexity: O(1) as we are not using any extra space
+class Solution:
+    def setZeroes(self, matrix: List[List[int]]) -> None:
+        m = len(matrix)
+        n = len(matrix[0])
+        first_row_zero = False
+        first_col_zero = False
+        for j in range (n):
+            if matrix[0][j] == 0:
+                first_row_zero = True
+                break
+        for i in range (m) :
+            if matrix[i][0] == 0:
+                first_col_zero = True
+                break
+        for i in range (1, m):
+            for j in range (1, n):
+                if matrix[i][j] == 0:
+                    matrix[i][0] = 0
+                    matrix[0][j] = 0
+        for i in range (1,m):
+            for j in range (1,n):
+                if matrix[i][0] == 0 or matrix[0][j] == 0:
+                    matrix[i][j] = 0
+        if first_col_zero:
+            for i in range (m):
+                matrix[i][0] = 0
+        if first_row_zero:
+            for j in range (n):
+                matrix[0][j] = 0
+        
