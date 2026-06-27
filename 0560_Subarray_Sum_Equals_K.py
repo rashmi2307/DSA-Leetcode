bb534c5c6aa3d6ed584d23921646aecaad10f5da
Rashmi Andhale <rashmiandhale23@gmail.com>
2026-06-27 22:00:17 +0530

    Subarray Sum Equaals K

---
+++ 0560_Subarray_Sum_Equals_K.py
@@ -0,0 +1,29 @@
+# Given an array of integers nums and an integer k, return the total number of subarrays whose sum equals to k.
+
+# A subarray is a contiguous non-empty sequence of elements within an array.
+
+# Example 1:
+# Input: nums = [1,1,1], k = 2
+# Output: 2
+
+# Example 2:
+# Input: nums = [1,2,3], k = 3
+# Output: 2
+
+
+
+# Brute Force Approach : Time Limit Exceeded
+# Time Complexity: O(n^2) where n is the length of the input array
+# Space Complexity: O(1) as we are using only constant space to store the count of subarrays
+from typing import List
+class Solution:
+    def subarraySum(self, nums: List[int], k: int) -> int:
+        count = 0
+        n = len(nums)
+        for i in range (n):
+            sum = 0
+            for j in range (i,n):
+                sum += nums[j]
+                if sum == k:
+                    count += 1
+        return count
