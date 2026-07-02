2e914bc2ce8f143674e03401b9d1df6a1c0f35e6
Rashmi Andhale <rashmiandhale23@gmail.com>
2026-07-02 23:16:33 +0530

    Reverse Pairs

--- 0493_Reverse_Pairs.py
+++ 0493_Reverse_Pairs.py
@@ -35,3 +35,51 @@ class Solution:
                 if nums[i] > 2* nums[j]:
                     count += 1
         return count
+    
+
+
+# Optimized Approach: Using merge sort technique to count the reverse pairs.
+# Time Complexity: O(nlogn) where n is the length of the input array
+class Solution:
+    def reversePairs(self, nums: List[int]) -> int:
+        return self.sort(nums,0,len(nums)-1)
+
+    def sort(self,nums,low,high) -> int:
+        count = 0
+        if low>=high:
+            return count
+        mid = (low+high)//2
+        count += self.sort(nums,low, mid)
+        count += self.sort(nums,mid+1,high)
+        count += self.countPairs(nums,low,mid,high)
+        self.merge(nums, low, mid, high)
+        return count
+
+    def merge(self,nums,low,mid,high):
+        temp = []
+        left = low
+        right = mid + 1
+        while left<=mid and right <=high:
+            if nums[left] <=nums[right]:
+                temp.append(nums[left])
+                left+=1
+            else:
+                temp.append(nums[right])
+                right+=1
+        while left<=mid:
+            temp.append(nums[left])
+            left+=1
+        while right<=high:
+            temp.append(nums[right])
+            right+=1
+        for i in range (low, high + 1):
+            nums[i] = temp[i - low]
+
+    def countPairs (self,nums, low, mid, high)-> int:
+        count = 0
+        right = mid + 1
+        for i in range (low,mid + 1):
+            while (right <=high and nums[i]>nums[right]*2):
+                right += 1
+            count += right - (mid + 1)
+        return count
