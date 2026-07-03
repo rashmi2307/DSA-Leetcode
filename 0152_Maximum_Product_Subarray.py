a200f327892ab4e5e6cd9260ae8d0dfbaaafc052
Rashmi Andhale <rashmiandhale23@gmail.com>
2026-07-03 23:00:26 +0530

    Maximum Product Subarray

--- 0152_Maximum_Product_Subarray.py
+++ 0152_Maximum_Product_Subarray.py
@@ -30,3 +30,22 @@ class Solution:
                 if product > maxp:
                     maxp = product
         return maxp
+    
+
+
+# Optimized Approach: Using dynamic programming technique to find the maximum product subarray.
+# Time Complexity: O(n) where n is the length of the input array
+class Solution:
+    def maxProduct(self, nums: List[int]) -> int:
+        n = len(nums)
+        prefix, suffix  = 1, 1
+        answer = float("-inf")
+        for i in range (n):
+            if prefix == 0:
+                prefix = 1
+            if suffix == 0:
+                suffix = 1
+            prefix *= nums[i]
+            suffix *= nums[n-i-1]
+            answer = max(prefix, suffix, answer)
+        return answer
