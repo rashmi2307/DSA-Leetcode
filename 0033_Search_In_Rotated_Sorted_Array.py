3e534d89c4c96e31ff2c587cae23b0d72520e096
Rashmi Andhale <rashmiandhale23@gmail.com>
2026-07-06 18:18:19 +0530

    Search In Rotated Sorted Array

---
+++ 0033_Search_In_Rotated_Sorted_Array.py
@@ -0,0 +1,63 @@
+# There is an integer array nums sorted in ascending order (with distinct values).
+
+# Prior to being passed to your function, nums is possibly left rotated at an unknown index k (1 <= k < nums.length) such that the resulting array is [nums[k], nums[k+1], ..., nums[n-1], nums[0], nums[1], ..., nums[k-1]] (0-indexed). For example, [0,1,2,4,5,6,7] might be left rotated by 3 indices and become [4,5,6,7,0,1,2].
+
+# Given the array nums after the possible rotation and an integer target, return the index of target if it is in nums, or -1 if it is not in nums.
+
+# You must write an algorithm with O(log n) runtime complexity.
+
+# Example 1:
+# Input: nums = [4,5,6,7,0,1,2], target = 0
+# Output: 4
+
+# Example 2:
+# Input: nums = [4,5,6,7,0,1,2], target = 3
+# Output: -1
+
+# Example 3:
+# Input: nums = [1], target = 0
+# Output: -1
+
+
+
+# Brute Force Approach:
+# Time Complexity: O(n) - We are iterating through the entire array to find the target integer.
+# Space Complexity: O(1) - We are not using any extra space.
+
+from typing import List
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        n = len(nums)
+        for i in range (n):
+            if target == nums[i]:
+                return i
+        return -1
+        
+
+
+
+# Binary Search Approach:
+# Time Complexity: O(log n) - We are iterating through the entire array to find the target integer.
+# Space Complexity: O(1) - We are not using any extra space.
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        low = 0
+        high = len(nums) - 1
+
+        while low <= high:
+            mid = (low+high)//2
+
+            if target == nums[mid]:
+                return mid
+
+            if nums[low] <= nums[mid]:
+                if nums[low] <= target < nums[mid]:
+                    high = mid - 1
+                else:
+                    low = mid + 1
+            else:
+                if nums[mid] < target <= nums[high]:
+                    low = mid + 1
+                else:
+                    high = mid - 1
+        return -1
