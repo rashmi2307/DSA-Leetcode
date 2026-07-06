ce695f9d00f7510e984601310ecdbcd0208eeed7
Rashmi Andhale <rashmiandhale23@gmail.com>
2026-07-06 18:35:07 +0530

    Search In Rotated Sortd Array II

--- 0081_Search_In_Rotated_Sorted_Array_II.py
+++ 0081_Search_In_Rotated_Sorted_Array_II.py
@@ -27,3 +27,38 @@ class Solution:
             if target == nums[i]:
                 return True
         return False
+    
+
+
+
+# Binary Search Approach:
+# Time Complexity: O(log n) - We are iterating through the entire array to find the target integer.
+# Space Complexity: O(1) - We are not using any extra space.
+class Solution:
+    def search(self, nums: List[int], target: int) -> bool:
+        n = len(nums)
+        low = 0
+        high = n - 1
+
+        while low <= high:
+            mid = (low+high)//2
+
+            if target == nums[mid]:
+                return True
+            
+            if nums[mid] == nums[low] == nums[high]:
+                low += 1
+                high -= 1
+                continue
+
+            if nums[low] <= nums[mid]:
+                if nums[low] <= target < nums[mid]:
+                    high = mid - 1
+                else:
+                    low = mid + 1
+            else:
+                if nums[mid] < target <= nums[high]:
+                    low = mid + 1
+                else:
+                    high = mid - 1
+        return False
