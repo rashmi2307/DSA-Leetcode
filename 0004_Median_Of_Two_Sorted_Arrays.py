efebbcd3578d37b902580c80cf79f10d6a6235eb
Rashmi Andhale <rashmiandhale23@gmail.com>
2026-07-13 00:19:54 +0530

    Median Of Two Sorted Arrays

--- 0004_Median_Of_Two_Sorted_Arrays.py
+++ 0004_Median_Of_Two_Sorted_Arrays.py
@@ -57,5 +57,36 @@ class Solution:
 # Optimized Approach:
 # Time Complexity: O(log(min(m,n))) - We are performing binary search on the smaller array to find the correct partition. The binary search will take log(min(m,n)) time, where m and n are the sizes of the two arrays.
 # Space Complexity: O(1) - We are not using any extra space.
+class Solution:
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        n1, n2 = len(nums1), len(nums2)
 
+        if n1 > n2:
+            return self.findMedianSortedArrays(nums2, nums1)
+
+        low = 0
+        high = n1
+        left = (n1 + n2 + 1)//2
+
+        while low <= high:
+            
+            mid1 = (low + high)//2
+            mid2 = left - mid1
+
+            l1 = float("-inf") if mid1 == 0 else nums1[mid1-1]
+            l2 = float("-inf") if mid2 == 0 else nums2[mid2-1]
+
+            r1 = float("inf") if mid1 == n1 else nums1[mid1]
+            r2 = float("inf") if mid2 == n2 else nums2[mid2]
+
+            if l1 <= r2 and l2 <= r1:
+
+                if (n1 + n2) % 2 == 1:
+                    return max(l1, l2)
+                return (max(l1, l2) + min(r1, r2))/2
+
+            elif l1 > r2:
+                high = mid1 - 1
+            else:
+                low = mid1 + 1
 
