6ba36832b810c717d32ff38805173282b8e6ddff
Rashmi Andhale <rashmiandhale23@gmail.com>
2026-07-16 22:13:55 +0530

    Remove Outermost Parentheses

---
+++ 1021_Remove_Outermost_Parentheses.py
@@ -0,0 +1,47 @@
+# A valid parentheses string is either empty "", "(" + A + ")", or A + B, where A and B are valid parentheses strings, and + represents string concatenation.
+
+# For example, "", "()", "(())()", and "(()(()))" are all valid parentheses strings.
+# A valid parentheses string s is primitive if it is nonempty, and there does not exist a way to split it into s = A + B, with A and B nonempty valid parentheses strings.
+
+# Given a valid parentheses string s, consider its primitive decomposition: s = P1 + P2 + ... + Pk, where Pi are primitive valid parentheses strings.
+
+# Return s after removing the outermost parentheses of every primitive string in the primitive decomposition of s.
+
+# Example 1:
+# Input: s = "(()())(())"
+# Output: "()()()"
+# Explanation: 
+# The input string is "(()())(())", with primitive decomposition "(()())" + "(())".
+# After removing outer parentheses of each part, this is "()()" + "()" = "()()()".
+
+# Example 2:
+# Input: s = "(()())(())(()(()))"
+# Output: "()()()()(())"
+# Explanation: 
+# The input string is "(()())(())(()(()))", with primitive decomposition "(()())" + "(())" + "(()(()))".
+# After removing outer parentheses of each part, this is "()()" + "()" + "()(())" = "()()()()(())".
+
+# Example 3:
+# Input: s = "()()"
+# Output: ""
+# Explanation: 
+# The input string is "()()", with primitive decomposition "()" + "()".
+# After removing outer parentheses of each part, this is "" + "" = "".
+
+
+
+
+class Solution:
+    def removeOuterParentheses(self, s: str) -> str:
+        result = ""
+        counter = 0
+        for i in s:
+            if i == "(":
+                counter += 1
+                if counter > 1:
+                    result += i
+            else:
+                counter -= 1
+                if counter > 0:
+                    result += i
+        return result
