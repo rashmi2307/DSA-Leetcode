21824650cc0c6567b21a7568c8043d520ce248cc
Rashmi Andhale <rashmiandhale23@gmail.com>
2026-08-08 18:49:28 +0530

    Middle of The Linked List

---
+++ 0876_Middle_Of_The_Linked_List.py
@@ -0,0 +1,55 @@
+# Given the head of a singly linked list, return the middle node of the linked list.
+
+# If there are two middle nodes, return the second middle node.
+
+# Example 1:
+# Input: head = [1,2,3,4,5]
+# Output: [3,4,5]
+# Explanation: The middle node of the list is node 3.
+
+# Example 2:
+# Input: head = [1,2,3,4,5,6]
+# Output: [4,5,6]
+# Explanation: Since the list has two middle nodes with values 3 and 4, we return the second one.
+ 
+
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+from typing import Optional
+from typing import ListNode
+class Solution:
+    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if head is None or head.next is None:
+            return head
+        temp = head
+        count = 0
+        while temp is not None:
+            count += 1
+            temp = temp.next
+        mid = count//2 + 1
+
+        temp = head
+
+        while temp is not None:
+            mid = mid - 1
+            if mid == 0:
+                break
+            temp = temp.next
+        return temp
+
+
+
+# Hare and tortoise approach, hare goes twice as fast as tortoise, when hare reaches the end of the list, tortoise will be at the middle of the list.
+class Solution:
+    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if head is None or head.next is None:
+            return head
+        slow, fast = head, head
+        while fast and fast.next and slow:
+            fast = fast.next.next
+            slow = slow.next
+        return slow
