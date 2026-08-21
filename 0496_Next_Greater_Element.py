1ea68401e2a9732a6467044626a1767029c9e046
Rashmi Andhale <rashmiandhale23@gmail.com>
2026-08-21 19:51:32 +0530

    Next Greater Element

---
+++ 0496_Next_Greater_Element.py
@@ -0,0 +1,49 @@
+# The next greater element of some element x in an array is the first greater element that is to the right of x in the same array.
+
+# You are given two distinct 0-indexed integer arrays nums1 and nums2, where nums1 is a subset of nums2.
+
+# For each 0 <= i < nums1.length, find the index j such that nums1[i] == nums2[j] and determine the next greater element of nums2[j] in nums2. If there is no next greater element, then the answer for this query is -1.
+
+# Return an array ans of length nums1.length such that ans[i] is the next greater element as described above.
+
+# Example 1:
+# Input: nums1 = [4,1,2], nums2 = [1,3,4,2]
+# Output: [-1,3,-1]
+# Explanation: The next greater element for each value of nums1 is as follows:
+# - 4 is underlined in nums2 = [1,3,4,2]. There is no next greater element, so the answer is -1.
+# - 1 is underlined in nums2 = [1,3,4,2]. The next greater element is 3.
+# - 2 is underlined in nums2 = [1,3,4,2]. There is no next greater element, so the answer is -1.
+
+# Example 2:
+# Input: nums1 = [2,4], nums2 = [1,2,3,4]
+# Output: [3,-1]
+# Explanation: The next greater element for each value of nums1 is as follows:
+# - 2 is underlined in nums2 = [1,2,3,4]. The next greater element is 3.
+# - 4 is underlined in nums2 = [1,2,3,4]. There is no next greater element, so the answer is -1.
+
+
+
+
+from git import List
+class Solution:
+    def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        stack = []
+        nge = {}
+        
+        for i in range(len(nums2)-1,-1,-1):
+
+            while stack and stack[-1] <= nums2[i] :
+                stack.pop()
+
+            if not stack:
+                nge[nums2[i]] = -1
+            else:
+                nge[nums2[i]] = stack[-1]
+
+            stack.append(nums2[i])
+
+        result  = []
+        for num in nums1:
+            result.append(nge[num])
+        
+        return result
